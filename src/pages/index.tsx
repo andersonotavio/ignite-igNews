bbb8f0b17f5a44bb61b601ace344a39e7c407d6e
andersonotavio <andersonotavio@hotmail.com>
2021-09-25 08:59:43 -0300

    estrutura da pagina

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+
+import Head from 'next/head'
+
+import styles from '../styles/home.module.scss'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>ig news</title>
+      </Head>
+      <h1>
+        Hello Word
+      </h1>
+    </>
+    
+  )
+}
