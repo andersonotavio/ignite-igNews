c2bdccb3820fe145bd40ba5b155454b2d5a4bee4
andersonotavio <andersonotavio@hotmail.com>
2021-10-12 18:12:50 -0300

    finished

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -31,7 +31,7 @@ export default function Posts({ posts }: PostsProps) {
         <div className={styles.posts}>
           {posts.map(post => (
             <Link href={`/posts/${post.slug}`}>
-              <a href="#" key={post.slug}>
+              <a key={post.slug}>
                 <time> {post.updatedAt} </time>
                 <strong> {post.title} </strong>
                 <p> {post.excerpt} </p>
